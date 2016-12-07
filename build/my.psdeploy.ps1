3fcbc647076f38f84cc1b84d78fdda36d0879f72
RamblingCookieMonster <ramblingcookiemonster@users.noreply.github.com>
2016-12-07 14:36:11 -0500

    name deployments

--- build/my.psdeploy.ps1
+++ build/my.psdeploy.ps1
@@ -1,11 +1,11 @@
 Deploy ExampleDeployment {
-    By FileSystem {
+    By FileSystem prod {
         FromSource "$ENV:BHProjectPath"
         To 'C:\Project'
         Tagged Prod
     }
 
-    By FileSystem {
+    By FileSystem local {
         FromSource "$ENV:Temp\Kitchen\data"
         To 'C:\Project'
         Tagged Local
